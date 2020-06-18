6cee4d7304b12086bc35b0b6008fcb5b23641e46
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-06-18 17:57:14 -0300

    My first commit, project created.

---
+++ insertion_sort.py
@@ -0,0 +1,15 @@
+#Name:         Roger Silva Santos Aguiar
+#Function:     Insertion Sort algorithm implementation
+#Initial date: June 18, 2020
+#Last update:  June 18, 2020
+
+#Variables
+
+#It creates an integer array
+my_array = (5, 2, 4, 6, 1, 3)
+
+#Process
+
+#It displays the array
+print(my_array)
+print(type(my_array))
