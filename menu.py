038803e9e2f85bc0b441c45b75ff69500e5efe52
Roger Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-06-20 22:49:39 -0300

    Menu file was created and a module was imported.

---
+++ menu.py
@@ -0,0 +1,38 @@
+#Name:         Roger Silva Santos Aguiar
+#Function:     This is the menu file, all other files will be executed from here
+#Initial date: June 20, 202
+#Last update:  June 20, 202
+
+#Required modules
+import insertion_sort
+
+class Menu:
+    # It creates two tuples
+    my_array = [5, 2, 4, 6, 1, 3]
+    option = 0
+
+    #Layout
+    print("**************************************Menu**************************************")
+
+    print("1 - Sort in ascending order")
+    print("2 - Sort in descending order")
+    print("3 - Exit")
+
+    #It asks the user to choose an option
+    option = int(input("Choose an option: "))
+    print("\n")
+
+    if option == 1:
+        #It sorts the array in ascending order
+        insertion_sort.Insertion_sort.insertionSortAsc(my_array)
+    elif option == 2:
+        #It sorts the array in descending order
+        insertion_sort.Insertion_sort.insertionSortDesc(my_array)
+    else:
+        print("End of the application")
+
+
+
+
+
+
