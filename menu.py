a38f7792da274331fb9a6f6622dedb62475ac619
Roger Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-06-21 11:07:43 -0300

    Layout updated, random_numbers module added, 2 new functions created to manipulate the imported modules.

--- menu.py
+++ menu.py
@@ -1,38 +1,55 @@
 #Name:         Roger Silva Santos Aguiar
 #Function:     This is the menu file, all other files will be executed from here
 #Initial date: June 20, 2020
-#Last update:  June 20, 2020
+#Last update:  June 21, 2020
 
 #Required modules
 import insertion_sort
+import random_numbers
 
 class Menu:
     # It creates two tuples
     my_array = [5, 2, 4, 6, 1, 3]
     option = 0
 
-    #Layout
-    print("**************************************Menu**************************************")
+    #This function generates and return an integer random list
+    def generate_integer_list(self):
+        print("This application generates a list with integer random numbers and sorts this one using Insertion Sort algorithm.")
+        print("Enter the following informations:\n")
 
-    print("1 - Sort in ascending order")
-    print("2 - Sort in descending order")
-    print("3 - Exit")
+        #Prompt
+        quantity = int(input("Enter the quantity of numbers you want to generate: "))
+        min = int(input("Enter the initial number: "))
+        max = int(input("Enter the last number: "))
 
-    #It asks the user to choose an option
-    option = int(input("Choose an option: "))
-    print("\n")
+        new_array = random_numbers.RandomNumbers(quantity, min, max)# It creates a new object
+        integer_list = new_array.generateRandomNumbers()     # It gets an integer list with random numbers
+        return  integer_list                                 # It returns the list
 
-    if option == 1:
-        #It sorts the array in ascending order
-        insertion_sort.Insertion_sort.insertionSortAsc(my_array)
-    elif option == 2:
-        #It sorts the array in descending order
-        insertion_sort.Insertion_sort.insertionSortDesc(my_array)
-    else:
-        print("End of the application")
+    #This function displays the options to the user
+    def display_layout(self, integer_list):
+        #Layout
+        print("\n**************************************Menu**************************************")
+        print("1 - Sort in ascending order")
+        print("2 - Sort in descending order")
+        print("3 - Exit")
 
+        #It asks the user to choose an option
+        option = int(input("Choose an option: "))
+        print("\n")
+        if option == 1:
+            # It sorts the array in ascending order
+            insertion_sort.Insertion_sort.insertionSortAsc(integer_list)
+        elif option == 2:
+            #It sorts the array in descending order
+            insertion_sort.Insertion_sort.insertionSortDesc(integer_list)
+        else:
+            print("End of the application")
 
-
-
+#If the application is executed directly, the following code will be executed.
+if __name__ == '__main__':
+    menu = Menu()#It creates an object
+    integer_list = menu.generate_integer_list()
+    menu.display_layout(integer_list)
 
 
